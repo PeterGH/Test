79ce692c6c58746de7e0dc6a8c303f16973bf702
Peter Zhou <peteryzufl@gmail.com>
2014-01-03 18:49:54 -0800

    D-ary Heap Sort

--- Algorithm/HeapSort.cpp
+++ Algorithm/HeapSort.cpp
@@ -16,4 +16,21 @@ namespace Test {
 
 		return h;
 	}
+
+	// (d^(h-1)-1)/(d-1) < length <= (d^h-1)/(d-1)
+	// There are h d-bits and the pattern is between:
+	// 1    0    0    0     ... 0    0    0
+	// (d-1)(d-1)(d-1)(d-1) ... (d-1)(d-1)(d-1)
+	unsigned int HeapSort::Height(unsigned int length, unsigned int d)
+	{
+		unsigned int x = length * (d - 1);
+		unsigned int h = 0;
+		while (x > 0) {
+			x = x / d;
+			h++;
+		}
+
+		return h;
+	}
+
 }
--- Algorithm/HeapSort.h
+++ Algorithm/HeapSort.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cmath>
 #include <stdexcept>
+#include "String.h"
 
 using namespace std;
 
@@ -21,12 +23,36 @@ namespace Test {
 		template<class T> static void Swap(T * input, unsigned int i, unsigned int j);
 		// Rearrange [i, length - 1] so that it is a heap. 0 <= i < length
 		// The assumption is the subtrees rooted at i are already heapified.
-		template<class T> static void Heapify(T * input, unsigned int i, unsigned int length);
+		template<class T> static void HeapifyElement(T * input, unsigned int i, unsigned int length);
+
+		// d-ary heap
+		//                                                  0
+		//                   1                              2                    ...          d
+		// (d+1)                   (d+2) ... (d+d) | (2d+1) (2d+2) ... (2d+d) | (d^2+1) (d^2+2) ... (d^2+d)
+		// (d^2+d+1) (d^2+d+2) ...
+		// ......
+		// Given height h, the number of nodes are [(d^(h-1)-1)/(d-1)+1, (d^h-1)/(d-1)]
+		// The indices at height h are [(d^(h-1)-1)/(d-1), (d^h-1)/(d-1)-1]
+
+		// Return the index of the parent of node i
+		static unsigned int Parent(unsigned int i, unsigned int d) { return (i - 1) / d; }
+		// Return the index of the j-th child of node i. j is zero based.
+		static unsigned int Child(unsigned int i, unsigned int j, unsigned int d) { return i * d + j + 1; }
+		__declspec(dllexport) static unsigned int Height(unsigned int length, unsigned int d);
+		// Rearrange [i, length - 1] so that it is a heap. 0 <= i < length
+		// The assumption is the subtrees rooted at i are already heapified.
+		template<class T> static void HeapifyElement(T * input, unsigned int i, unsigned int length, unsigned int d);
+
 	public:
 		// Construct the array into a max heap from bottom up
 		template<class T> static void Heapify(T * input, unsigned int length);
 		template<class T> static void ParallelHeapify(T * input, unsigned int length);
 		template<class T> static void Sort(T * input, unsigned int length);
+
+		// Construct the array into a max d-ary heap from bottom up
+		template<class T> static void Heapify(T * input, unsigned int length, unsigned int d);
+		template<class T> static void ParallelHeapify(T * input, unsigned int length, unsigned int d);
+		template<class T> static void Sort(T * input, unsigned int length, unsigned int d);
 	};
 
 	template<class T> void HeapSort::Swap(T * input, unsigned int i, unsigned int j)
@@ -37,7 +63,7 @@ namespace Test {
 		input[j] = temp;
 	}
 
-	template<class T> void HeapSort::Heapify(T * input, unsigned int i, unsigned length)
+	template<class T> void HeapSort::HeapifyElement(T * input, unsigned int i, unsigned length)
 	{
 		if (i >= length) return;
 		unsigned int max = i;
@@ -53,26 +79,28 @@ namespace Test {
 
 		if (max != i) {
 			Swap(input, i, max);
-			Heapify(input, max, length);
+			HeapifyElement(input, max, length);
 		}
 	}
 
 	template<class T> void HeapSort::Heapify(T * input, unsigned int length)
 	{
 		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
 
 		unsigned int height = Height(length);
 
 		// The elements at bottom are indexed in [2^(height - 1) - 1, 2^height - 2]
 		// We only need to heapify elements above them
 		for (long i = ((1 << (height - 1)) - 2); i >= 0; i--) {
-			Heapify(input, (unsigned int)i, length);
+			HeapifyElement(input, (unsigned int)i, length);
 		}
 	}
 
 	template<class T> void HeapSort::ParallelHeapify(T * input, unsigned int length)
 	{
 		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
 
 		unsigned height = Height(length);
 
@@ -82,7 +110,7 @@ namespace Test {
 				unsigned int((1 << (h - 1)) - 1),
 				unsigned int((1 << h) - 1),
 				[&](unsigned int i) {
-					Heapify(input, (unsigned int)i, length);
+					HeapifyElement(input, (unsigned int)i, length);
 				}
 			);
 		}
@@ -91,6 +119,7 @@ namespace Test {
 	template<class T> void HeapSort::Sort(T * input, unsigned int length)
 	{
 		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
 
 		// Make a heap
 		Heapify(input, length);
@@ -101,7 +130,79 @@ namespace Test {
 			// The range [i, length - 1] is sorted.
 			Swap(input, 0, (unsigned int)i);
 			// Rearrange [0, i - 1] so that it is a heap
-			Heapify(input, 0, (unsigned int)i);
+			HeapifyElement(input, 0, (unsigned int)i);
+		}
+	}
+
+	// Rearrange [i, length - 1] so that it is a heap. 0 <= i < length
+	// The assumption is the subtrees rooted at i are already heapified.
+	template<class T> void HeapSort::HeapifyElement(T * input, unsigned int i, unsigned int length, unsigned int d)
+	{
+		if (i >= length) return;
+		unsigned int max = i;
+
+		for (unsigned int j = 0; j < d; j++) {
+			unsigned int c = Child(i, j, d);
+			if (c < length && input[c] > input[max]) {
+				max = c;
+			}
+		}
+
+		if (max != i) {
+			Swap(input, i, max);
+			HeapifyElement(input, max, length, d);
+		}
+	}
+
+	template<class T> void HeapSort::Heapify(T * input, unsigned int length, unsigned int d)
+	{
+		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
+		if (d <= 1) throw invalid_argument(String::Format("d %d is invalid", d));
+
+		unsigned int height = Height(length, d);
+		long long index = ((long long)pow(d, height - 1) - 1) / (d - 1) - 1;
+		for (long long i = index; i >= 0; i--) {
+			HeapifyElement(input, (unsigned int)i, length, d);
+		}
+	}
+
+	template<class T> void HeapSort::ParallelHeapify(T * input, unsigned int length, unsigned int d)
+	{
+		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
+		if (d <= 1) throw invalid_argument(String::Format("d %d is invalid", d));
+
+		unsigned int height = Height(length, d);
+
+		for (long long h = height - 1; h > 0; h--) {
+			// For h, the index is in [(d ^ (h - 1) - 1) / (d - 1), (d^h - 1) / (d - 1) - 1]
+			parallel_for(
+				unsigned int((pow(d, h - 1) - 1) / (d - 1)),
+				unsigned int((pow(d, h) - 1) / (d - 1)),
+				[&](unsigned int i) {
+					HeapifyElement(input, (unsigned int)i, length, d);
+				}
+			);
+		}
+	}
+
+	template<class T> void HeapSort::Sort(T * input, unsigned int length, unsigned int d)
+	{
+		if (input == nullptr)  throw invalid_argument("input is nullptr");
+		if (length <= 0) throw invalid_argument(String::Format("length %d is invalid", length));
+		if (d <= 1) throw invalid_argument(String::Format("d %d is invalid", d));
+
+		// Make A a heap
+		Heapify(input, length, d);
+
+		// Sort
+		for (long i = length - 1; i >= 0; i--) {
+			// Swap the current maximum value, which is at position 0, to position i.
+			// The range [i, length - 1] is sorted.
+			Swap(input, 0, (unsigned int)i);
+			// Rearrange [0, i - 1] so that it is a heap
+			HeapifyElement(input, 0, (unsigned int)i, d);
 		}
 	}
 }
